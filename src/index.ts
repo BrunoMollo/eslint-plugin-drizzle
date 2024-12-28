f9caf38758ab34c785cc0c70a038717e3fbc2570
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-12-28 01:50:56 -0300

    expose rule and added to recomended and all

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type { TSESLint } from "@typescript-eslint/utils";
 import deleteRule from "./enforce-delete-with-where";
 import updateRule from "./enforce-update-with-where";
+import logicalOperatorsRule from "./no-logical-operators-in-where";
 import { name, version } from "../package.json";
 import all from "./configs/all";
 import recommended from "./configs/recommended";
@@ -8,6 +9,7 @@ import recommended from "./configs/recommended";
 export const rules = {
   "enforce-delete-with-where": deleteRule,
   "enforce-update-with-where": updateRule,
+  "no-logical-operators-in-where": logicalOperatorsRule,
 } satisfies Record<string, TSESLint.RuleModule<string, Array<unknown>>>;
 
 export const configs = { all, recommended };
